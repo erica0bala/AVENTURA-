6ae440795333a5ff9e8e4546325c6c7eff2d6dab
erica0bala <erica.ferro@escola.pr.gov.br>
2025-09-04 10:58:04 -0300

    ok

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = document.getElementById('but-reiniciar');
 
-avanca.forEach(button =>
-button.addEventListener('click')   
-)
+avanca.forEach(button =>{
+button.addEventListener('click',function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+
+    atual.classList.remove('ativo');
+    const proximoElemento= document.getElementById(proximoPasso);
+
+    if(proximoElemento{
+        proximoElemento.classList.add('ativo');
+    } else {
+        console.error(`elemento com ID "${proximoPasso}"  não encontrado.');
+    }
+ });   
+});
 
