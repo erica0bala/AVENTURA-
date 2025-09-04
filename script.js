505b9eb5d4e1f0554e9c82e205a5abfb0f2bab95
erica0bala <erica.ferro@escola.pr.gov.br>
2025-09-04 09:58:59 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+const reiniciarBtn = document.getElementById('but-reiniciar');
 
-avanca.for
+avanca.forEach(button =>
+button.addEventListener     
+)
+
